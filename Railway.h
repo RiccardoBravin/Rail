bcc5d214288356563c2caae561d70a8f7cb4526d
EliaFeltrin <elia.feltrin@studenti.unipd.it>
2020-12-29 12:04:30 +0100

    aggiunta commenti doxygen

--- Railway.h
+++ Railway.h
@@ -7,27 +7,101 @@
 
 class Railway {
     public:
+        /**
+         * @brief Construct a new Railway object
+         * 
+         * @param line_description input file
+         */
         Railway(std::string line_description);
+        /**
+         * @brief default constructor (useless)
+         * 
+         */
         Railway();
+        /**
+         * @brief copy constructor
+         * 
+         */
         Railway(const Railway& rw);
+        /**
+         * @brief move constructor
+         * 
+         */
         Railway(Railway&& rw);
+        /**
+         * @brief destroyer
+         * 
+         */
         ~Railway() {};
 
+        /**
+         * @brief coparison operator
+         * 
+         */
         bool operator==(const Railway& rw);
+        /**
+         * @brief move operator
+         * 
+         */
         Railway& operator=(Railway&& rw);
+        /**
+         * @brief copy operator
+         * 
+         */
         Railway operator=(const Railway& rw);
-
+        /**
+         * @brief Get the beginning station of this railway
+         * 
+         * @return Station 
+         */
         Station get_beginning_station() const;
+        /**
+         * @brief Get the terminal station of this railway
+         * 
+         * @return Station 
+         */
         Station get_terminal_station() const;
+        /**
+         * @brief Get the station by index
+         * 
+         * @param i 
+         * @return Station 
+         */
         Station get_station(int i) const;
+        /**
+         * @brief Get the station by distance (rounded)
+         * 
+         * @param distance 
+         * @return Station 
+         */
         Station get_station_at_distance(int distance) const;
+        /**
+         * @brief how many station in this railway
+         * 
+         * @return int 
+         */
         int get_station_number() const;
 
     private:
+        /**
+         * @brief standard vector containing all station in this railway
+         * 
+         */
         std::vector<Station> stations;
+        /**
+         * @brief input file
+         * 
+         */
         std::string line_description_file;
-        void update_line_description();
+        /**
+         * @brief how many station in this railway
+         * 
+         */
         int station_number;
 };
 
+/**
+ * @brief output stream operator
+ * 
+ */
 std::ostream& operator<<(std::ostream& os, Railway& rw);
--- TimeTable.h
+++ TimeTable.h
@@ -5,6 +5,10 @@
 #include <iostream>
 #include "Railway.h"
 
+/**
+ * @brief contains time informations for each train
+ * 
+ */
 struct time_table_element{
     int train_number;
     bool departure_from_beginning;
@@ -14,21 +18,69 @@ struct time_table_element{
 
 class TimeTable{
     public:
+        /**
+         * @brief Construct a new Time Table object
+         * 
+         * @param time_table input file
+         * @param reference to belonging railway istance
+         */
         TimeTable(std::string time_table, const Railway& reference);
+        /**
+         * @brief copy constructor
+         * 
+         */
         TimeTable(const TimeTable& TT);
+        /**
+         * @brief move constructor
+         * 
+         */
         TimeTable(TimeTable&& TT);
+        /**
+         * @brief destroyer
+         * 
+         */
         ~TimeTable() {};
 
+        /**
+         * @brief comparison operator
+         * 
+         */
         bool operator==(const TimeTable& TT);
+        /**
+         * @brief move operator
+         * 
+         */
         TimeTable& operator=(TimeTable&& TT);
+        /**
+         * @brief copy operator
+         * 
+         */
         TimeTable operator=(const TimeTable& TT);
 
+        /**
+         * @brief Get the time table element by train number
+         * 
+         * @param train_number 
+         * @return time_table_element 
+         */
         time_table_element get_time_table_element(int train_number);
+        /**
+         * @brief container for time informations
+         * 
+         */
         std::vector<time_table_element> time_table;
         
 
     private:
+        /**
+         * @brief refernce to belonging railway istance
+         * 
+         */
         Railway& rw_reference;
 };
 
+/**
+ * @brief output stream operator
+ * 
+ */
 std::ostream& operator<<(std::ostream& os, const TimeTable& TT);
