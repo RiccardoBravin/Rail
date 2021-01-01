66c79dead9a140b4b0d22ff35c0c4ff109b16361
EliaFeltrin <elia.feltrin@studenti.unipd.it>
2021-01-01 20:04:53 +0100

    ho finito tranne per le cose che non so

--- Railway.cpp
+++ Railway.cpp
@@ -50,9 +50,9 @@ Railway::Railway(const Railway& rw){
     stations.clear();
     for(int i=0; i<rw.get_station_number(); i++) {
         if(rw.get_station(i).get_station_type() == 0) {
-            stations.push_back(&rw.get_station(i));
+            stations.push_back(new Principal(rw.get_station(i).get_station_name(), rw.get_station(i).get_station_distance()));
         } else {
-            stations.push_back(&rw.); 
+            stations.push_back(new Secondary(rw.get_station(i).get_station_name(), rw.get_station(i).get_station_distance()));
         }    
     }
 }
@@ -61,7 +61,7 @@ Railway::Railway(Railway&& rw){
 
 }
 
-Railway reverse (const Railway& rw){
+Railway reverse (Railway& rw){
     Railway temp;
     temp.set_source_file(rw.get_source_file_name());
     
@@ -69,60 +69,100 @@ Railway reverse (const Railway& rw){
     for(int i=rw.get_station_number()-1; i>-1; i--) {
         temp.add_station(rw.get_station(i));
     }
+
+    rw.set_reverse_reference(temp);
+    return temp;
 }
 
-Railway::bool operator==(const Railway& rw){
+//manca Station::operator!=(const Staion& st)
+/*bool Railway::operator==(const Railway& rw){
+    if(line_description_file_name != rw.get_source_file_name()))
+        return false;
+    if(num_stations != rw.get_station_number())
+        return false;
+    for(int i=0; i<num_stations; i++) {
+        if(rw.get_station(i).get_station_type() != get_station(i).get_station_type())
+            return false;
+    }
+    for(int i=0; i<num_stations; i++) {
+        *(stations[i]) != rw.get_station(i);
+    }
+}*/
 
-}
-
-Railway::Railway& operator=(Railway&& rw){
+/*Railway& Railway::operator=(Railway&& rw){
 
 }
 
 Railway::Railway operator=(const Railway& rw){
 
-}
+}*/
 
 void Railway::verify_railway(){
 
 }
 
-Railway::Station remove_station(int i){
-
+void Railway::remove_station(int i){
+    std::cout << "This station was removed:\n" << get_station(i); 
+    delete stations[i];
+    stations.erase(stations.begin() + i - 1);
 }
 
-Railway::Station get_beginning_station() const{
-
+Station& Railway::get_beginning_station() const{
+    return *(stations[0]);
 }
 
-Railway::Station get_terminal_station() const{
-
+Station& Railway::get_terminal_station() const{
+    return *(stations[stations.size()-1]);
 }
 
-Railway::Station get_station(int i) const{
-
+Station& Railway::get_station(int i) const{
+    return *(stations[i]);
 }
 
-Railway::Station get_station_at_distance(int distance) const{
-
+/*Station& Railway::get_station_at_distance(int distance) const{
+    return 0;
 }
 
-Railway::int get_station_number() const{
+Station& Railway::get_station(std::string name){
+    for(int i=0; i<stations.size(); i++) {
+        if(get_station(i).get_station_name() == name)
+            return *stations[i];
+    }
+    return nullptr;
+}*/
 
+int Railway::get_station_number() const{
+    return num_stations;
 }
 
-std::string get_source_file_name() const{
-
+std::string Railway::get_source_file_name() const{
+    return line_description_file_name;
 }
 
-Railway::void add_station(const Station& st){
+/*void Railway::add_station(const Station& st){
+    stations.push_back(che ci metto qua dentro???);
+}*/
 
-}
-
-Railway::void set_source_file(std::string line_description){
-
+void Railway::set_source_file(std::string line_description){
+    line_description_file_name= line_description;
 }
 
 std::ostream& operator<<(std::ostream& os, Railway& rw){
+    os << "Railway generated from " << rw.get_source_file_name() << ".\n";
+    if(rw.get_reverse_reference() == nullptr)
+        os << "This is the going railway ";
+    else 
+        os << "This is the returning railway ";
+    os << rw.get_beginning_station() << "-" << rw.get_terminal_station();
+    for(int i=0; i<rw.get_station_number(); i++)
+        os << rw.get_station(i);
+}
 
+
+void Railway::set_reverse_reference(Railway* ref){
+    reverse_railway = ref;
+}
+
+Railway* Railway::get_reverse_reference() {
+    return reverse_railway;
 }
