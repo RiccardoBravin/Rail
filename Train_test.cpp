2cf316b173c17f64a41c34a7ba563de46bdfdd12
RiccardoBravin <riccardo.bravin.1@studenti.unipd.it>
2021-01-01 22:31:01 +0100

    Fixed train destructor

--- Train_test.cpp
+++ Train_test.cpp
@@ -29,11 +29,19 @@ void test(vector<Train*> &v){
 }
 
 
+void train_v_des(vector<Train*> &v){
+    for(int i = 0; i < v.size(); i++){
+        v[i]->~Train();
+    }
+}
+
 int main(int argc, char const *argv[])
 {
 
     vector<Train*> v;
     test(v);
+
+    
 
     cout << *v[0];
 
@@ -91,9 +99,8 @@ int main(int argc, char const *argv[])
         cin.get();
     }
     
+    train_v_des(v);
     
-    
-
     SetConsoleTextAttribute(hConsole, 15);
 
     return 0;
