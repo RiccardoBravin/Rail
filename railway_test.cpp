a201e1d30775ada06545eae8bb9090ccb48b69cc
EliaFeltrin <elia.feltrin@studenti.unipd.it>
2021-01-09 14:41:51 +0100

    funziona tranne se i treni sono tutti di ritorno

--- railway_test.cpp
+++ railway_test.cpp
@@ -17,23 +17,29 @@ int main() {
         
     } catch(runtime_error e) {cout << e.what();}
 
-
+    if(tables.size() == 2) {
     railways.push_back(Railway(line_description, &tables[0]));
     railways.push_back(Railway());
     railways[1].reverse(railways[0], &tables[1]);
+    } else if(tables[0].is_going()) {
+        railways.push_back(Railway(line_description, &tables[0]));
+    } else {
+        railways.push_back(Railway(line_description, &tables[0]));
+        railways[0].reverse(railways[0], &tables[0]);
+    }
 
-    //cout << railways[0] << tables[0] << endl << endl << railways[1] << tables[1] <<  endl;
+    //for(int i=0; i<railways.size(); i++)  cout << railways[i] << endl << tables[i] << endl << endl;
 
     
     cout << "____________ADJUST_TIMETABLE__________" << endl;
     for(int i=0; i<tables.size(); i++) {
         tables[i].adjust_timetable(railways[0].get_principal_count(), railways[0].get_station_count());
     }
-    //cout << railways[0] << tables[0] << endl << endl << railways[1] << tables[1] <<  endl;
+    for(int i=0; i<railways.size(); i++)  cout << railways[i] << endl << tables[i] << endl << endl;
 
     cout << "____________VERIFY_RAILWAY__________" << endl;
         railways[0].verify_railway();
-    //cout << railways[0] << tables[0] << endl << endl << railways[1] << tables[1] <<  endl;
+    //for(int i=0; i<railways.size(); i++)  cout << railways[i] << endl << tables[i] << endl << endl;
     
 
     
